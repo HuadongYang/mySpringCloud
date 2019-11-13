90fab1e9e6c31dd4ff1a8a02b98c7ef2dd5d5ae5
zhanghuanhuan <zhanghuanhuan@inspiry.cn>
2019-11-13 16:14:53 +0800

    hystrix 注解测试

--- service/tools-service/src/main/java/com/my/cloud/hystrix/api/HystrixTestApi.java
+++ service/tools-service/src/main/java/com/my/cloud/hystrix/api/HystrixTestApi.java
@@ -1,15 +1,24 @@
 package com.my.cloud.hystrix.api;
 
+import com.my.cloud.hystrix.service.HiService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("hystrix test")
 @RestController
 @RequestMapping("/hy/test")
 public class HystrixTestApi {
 
+    @Autowired
+    HiService hiService;
+
+    @ApiOperation("hi")
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
-    public String hi(){
-        return "hi";
+    public String hi() throws InterruptedException {
+        return hiService.hi();
     }
 }
--- service/tools-service/src/main/java/com/my/cloud/hystrix/service/HiService.java
+++ service/tools-service/src/main/java/com/my/cloud/hystrix/service/HiService.java
@@ -1,11 +1,24 @@
 package com.my.cloud.hystrix.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HiService {
 
-    public String hi(){
+    @HystrixCommand(fallbackMethod = "timeoutFallback", threadPoolProperties = {
+            @HystrixProperty(name = "coreSize", value = "1"),
+            @HystrixProperty(name = "queueSizeRejectionThreshold", value = "20")
+    }, commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "8")
+    })
+    public String hi() throws InterruptedException {
+        Thread.sleep(200);
         return "hi";
+    }
+
+    public String timeoutFallback() {
+        return "timeout 降级";
     }
 }
